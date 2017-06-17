5d9e210bccb7e9163620aef487df9ba89e3101a1
Keenan Goldis <keenangoldis@gmail.com>
2017-06-17 18:09:55 -0500

    update App.js API

--- src/App.js
+++ src/App.js
@@ -1,28 +1,42 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+// import logo from './logo.svg';
 import axios from 'axios';
 import './App.css';
 
 class App extends Component {
+  constructor(props){
+    super(props)
+    this.state = {
+      apiPic: "",
+      apiExplanation: "",
+    }
+  }
+
   componentWillMount() {
-    axios.get('https://api.nasa.gov/mars-photos/api/v1/rovers/curiosity/photos?sol=1000&api_key=J8yUtPsCKTDCcfBfw4pO3jGmYM7OjJpQhgHBqTTb')
-    .then(function (response) {
+    axios.get('https://api.nasa.gov/planetary/apod?api_key=J8yUtPsCKTDCcfBfw4pO3jGmYM7OjJpQhgHBqTTb')
+    .then ((response) => {
       console.log(response);
+      this.setState({apiPic: response.data.hdurl})
+      this.setState({apiExplanation: response.data.explanation})
     })
-    .catch(function (error) {
+    .catch((error) => {
       console.log(error);
     });
   }
+
   render() {
     return (
       <div className="App">
         <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
+          <h2>Your Daily Planet Picture!</h2>
         </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+
+        <div className="Central">
+          <img src={this.state.apiPic} className="App-logo" alt="logo" />
+          <div className="Explanations">
+            <p> {this.state.apiExplanation} </p>
+          </div>
+        </div>
       </div>
     );
   }
